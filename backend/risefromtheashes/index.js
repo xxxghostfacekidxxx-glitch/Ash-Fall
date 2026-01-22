f5f80560d4f8c0235029e8fcdd0f5faafbc714c6
Knightfall <xxxghostfacekidxxx@gmail.com>
2026-01-21 19:09:17 -0600

    Update index.js

--- backend/risefromtheashes/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env) {
-    // simple response to confirm worker is live
-    return new Response("risefromtheashes Worker is running", {
-      headers: { "Content-Type": "text/plain" }
-    });
-  }
-};
